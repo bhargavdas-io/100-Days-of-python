77d0eac96b219c752864c1fe88022da45a79a44d
Bhargav Das <55290652+bhargavdas-io@users.noreply.github.com>
2024-06-23 01:43:03 +0530

    Update day_3_love_calculator.py

--- day_3_love_calculator.py
+++ day_3_love_calculator.py
@@ -20,7 +20,7 @@ e = lower_names.count("e")
 second_digit = l+o+v+e
 score = int(str(first_digit)+str(second_digit)) ###
 
-if (score < 10) or (score > 90) :
+if (score > 10) or (score < 90) :
     print(f"Your score is {score}, you go like coke and mentos.")
 elif (score >= 40) and (score <= 50):
     print(f"Your score is {score}, you are alright together.")
