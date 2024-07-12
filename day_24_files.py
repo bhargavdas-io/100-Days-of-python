62710f83852c27051edb4cb67b7b4b04db52859a
Bhargav Das <bhargavdas77@hotmail.com>
2024-07-12 15:15:19 +0530

    Day_24 Complete Files,Folders and Paths

--- day_24_files.py
+++ day_24_files.py
@@ -1,6 +1,8 @@
-with open("file.txt", mode="w") as file:
-    file.write("Hello, today is day 24\nThis class is on files and folders")
+with open(r"C:\Users\Bhargav\Desktop\file.txt", mode="w") as file:
+    file.write("Hello, today is day 25\nThis class is on files and folders")
 
 
 ##It's important to close a file after we are done with it., We can also
 ##use the with keyword, and this manages the file automatically to close.
+
+# r before the " in the string, converts it to raw string, for paths.
